2ef6ce84c777fa1cc2aa115986a05056849daff0
Polarcraft <ygtripps@outlook.com>
2016-09-12 22:12:28 -0500

    Create server.sh

---
+++ server.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+screen="changeme"
+pid=`ps aux | grep $screen | awk '{print $2}'`
+directory="/directory/to/server"
+javac="java -jar server.jar"
+command="screen -A -m -d -S $screen $javac"
+if ps ax | grep -v grep | grep $screen > /dev/null
+then
+    read -p "Server is running, do you want to stop your server? [Y/n]" resp
+    if [[ "$resp" == "y" || "$resp" == "Y" ]]; then
+        read -p "Do you want to restart the server? [Y/n]" resp2
+        if [[ "$resp2" == "y" || "$resp2" == "Y" ]]; then
+            echo "Restarting your server now."
+            kill -9 $pid 2>/dev/null
+            screen -wipe
+            eval "cd $directory && $command"
+        else
+            echo "Stopping your server now as requested."
+            kill -9 $pid 2>/dev/null
+            screen -wipe
+        fi
+    fi
+else
+    echo "Starting your server now as requested."
+    eval "cd $directory && $command"
+fi
